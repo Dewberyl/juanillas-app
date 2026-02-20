fe7868f2f4c154f1647a77438203ac30231490bc
Dewberyl <juanillasiee@email.com>
2026-02-21 02:51:56 +0800

    Exercise #2 - Laravel Architectural Concepts

---
+++ app/Providers/UserServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Services\UserService;
+
+class UserServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->singleton(UserService::class, function ($app) {
+            $users = [
+                [
+                    'name' => 'John Doe',
+                    'gender' => 'male'
+                ],
+                [
+                    'name' => 'Jamie Doe',
+                    'gender' => 'female'
+                ]
+            ];
+            return new UserService($users);
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Services\UserService;
+use Illuminate\Support\Facades\Response;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('show-users', [\App\Http\Controllers\UserController::class, 'show']);
+
+//service container
+Route::get('/test-container', function (Request $request) {
+    $input = $request->input('key');
+    return $input;
+});
+
+//service providers
+Route::get('/test-provider', function (UserService $userService) {
+    return $userService->listUsers();
+});
+
+//service Provider
+Route::get('/test-users', [UserController::class, 'index']);
+
+// Facades
+Route::get('test-facade', function (UserService $userService) {
+    return Response::json($userService->listUsers());
 });
